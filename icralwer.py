56fdc425ff58d30c57575992ca5e01ec5a4f9214
jungsehwan <jungsehwan0612@gmail.com>
2018-11-15 18:06:16 +0900

    정세환

---
+++ icralwer.py
@@ -0,0 +1,6 @@
+from icrawler.builtin import GoogleImageCrawler
+
+attractionList = ['tower eiffel', 'statue of liberty', 'niagara falls']
+for keyword in attractionList:
+    google_crawler = GoogleImageCrawler(parser_threads=2, downloader_threads=4, storage={'root_dir': 'images/{}'.format(keyword)})
+    google_crawler.crawl(keyword=keyword, max_num=5, min_size=(500,500))
